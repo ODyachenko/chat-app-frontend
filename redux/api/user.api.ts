6c09f73b4d64c637b40768c173e64aacf337d0dd
ODyachenko <oleg1696@gmail.com>
2023-09-13 11:57:06 +0300

    Realized user API

---
+++ redux/api/user.api.ts
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { iUserReg, iUserLog } from '@/types';
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  tagTypes: ['user'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:5555',
+  }),
+  endpoints: (builder: any) => ({
+    getAuthUser: builder.query({
+      query: () => ({
+        url: `/auth/me`,
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      }),
+      providesTags: ['user'],
+    }),
+    createUser: builder.mutation({
+      query: (body: iUserReg) => ({
+        url: `/auth/registration`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['user'],
+    }),
+    loginUser: builder.mutation({
+      query: (body: iUserLog) => ({
+        url: `/auth/login`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['user'],
+    }),
+  }),
+});
+
+export const {
+  useCreateUserMutation,
+  useLoginUserMutation,
+  useGetAuthUserQuery,
+} = userApi;
---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface iUserReg {
+  email: string;
+  password: string;
+  avatarUrl: string;
+  fullName: string;
+}
+export interface iUserLog {
+  email: string;
+  password: string;
+}
